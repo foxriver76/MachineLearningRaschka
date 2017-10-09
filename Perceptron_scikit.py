c2fd98d717ba842e78eab2312947da0c5ef99fc6
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-10-09 20:35:05 +0200

    predicted iris with the scikit-learn perceptron and evaluated with accuracy score

--- Perceptron_scikit.py
+++ Perceptron_scikit.py
@@ -11,6 +11,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
 
 """import iris dataset and splitting data"""
 iris = datasets.load_iris()
@@ -29,4 +30,13 @@ X_test_std = sc.transform(X_test)
 -------------------
     trough implemented "one vs rest" method we 
     can do multiple class classification (more than two classes)"""
-    
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+"""predict test data"""
+y_pred = ppn.predict(X_test_std)
+print('Fehlklassifizierte Exemplare: %d' \
+      % (y_test != y_pred).sum())
+
+"""evaluation via percentage of correct classified objects"""
+print('Korrektklassifizierungsrate: %.2f' % accuracy_score(y_test, y_pred))
