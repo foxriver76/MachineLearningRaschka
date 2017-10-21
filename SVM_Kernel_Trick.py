02c5cdf9d19ac3215080b4915bd28575a017fba4
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-10-21 15:49:22 +0200

    added preparation data for svm with kernel trick

---
+++ SVM_Kernel_Trick.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 15:43:02 2017
+
+@author: moritz
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""random Datensatz erzeugen mit random Labels für nichtlineare SVM (Kernel Trick)"""
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:,0] > 0, X_xor[:,1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+plt.scatter(X_xor[y_xor==1, 0], X_xor[y_xor==1, 1],
+            c='b', marker='x', label='1')
+plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1],
+            c='r', marker='s', label='-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
