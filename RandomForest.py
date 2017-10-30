5383bf50f1cc226464535624f97533d6624f0ca9
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-10-30 16:37:48 +0100

    added Random Forest Classifier and plotted decisionm regions of iris

---
+++ RandomForest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 16:34:14 2017
+
+@author: moritz
+"""
+
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from plot_decision_regions import plot_decision_regions
+from sklearn.ensemble import RandomForestClassifier
+
+
+"""import iris dataset and splitting data"""
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0) #30% Testdaten, 70% Trainingsdaten
+
+forest = RandomForestClassifier(criterion='entropy',
+                                n_estimators=10,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=forest, test_idx=range(105, 150))
+plt.xlabel('Länge des Blütenblattes [cm]')
+plt.ylabel('Breite des Blütenblattes [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+
