2e05873355114672c77bb1046781956dae624d81
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-11-06 13:48:52 +0100

    added strategy to fill NaN values with median

--- Data Preparation/missing_values.py
+++ Data Preparation/missing_values.py
@@ -8,6 +8,7 @@ Created on Sat Nov  4 14:53:27 2017
 
 import pandas as pd
 from io import StringIO
+from sklearn.preprocessing import Imputer
 
 """Beim einlesen zwischen den Kommata keine Leerzeichen sonst werden diese in die String/Char Labels integriert"""
 csv_data = '''A,B,C,D
@@ -17,6 +18,7 @@ csv_data = '''A,B,C,D
     
 df = pd.read_csv(StringIO(csv_data))
 
+"""Strategien zum entfernen von Missing Values Attributen"""
 """Anzahl fehlende Werte pro Spalte"""
 #print(df.isnull().sum()) 
 
@@ -37,3 +39,15 @@ df = pd.read_csv(StringIO(csv_data))
 
 """Nur Zeilen löschen, in denen NaN in einer bestimmten Spalte (hier: 'C') vorkommt"""
 #print(df.dropna(subset=['C']))
+
+"""Fehlende Werte ergänzen"""
+"""Mittelwerte der Merkmalsspalte einfügen"""
+imr = Imputer(missing_values='NaN', 
+              strategy='mean', axis=0)
+"""andere Möglichkeiten sind 'median' oder 'most_frequent' wobei sich 'most_frequent für kategoriale Werte eignet""" 
+imr = imr.fit(df)
+imputed_data = imr.transform(df.values)
+#print(imputed_data)
+
+
+
