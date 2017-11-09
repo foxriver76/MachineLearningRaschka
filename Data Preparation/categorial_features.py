b29d95c97420633a3890eb3d72212c75b8ca92b3
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-11-09 10:49:24 +0100

    added class label tranform

--- Data Preparation/categorial_features.py
+++ Data Preparation/categorial_features.py
@@ -7,6 +7,8 @@ Created on Wed Nov  8 21:22:47 2017
 """
 
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 """Dataframe mit nominalen (color), ordinalem (Größe) und numersichen (price) Feature"""
 df = pd.DataFrame([
@@ -25,3 +27,18 @@ size_mapping = {
 df['size'] = df['size'].map(size_mapping)
 
 inv_size_mapping = {v: k for k, v in size_mapping.items()}
+
+"""Umcodieren von Klassenlabels in Ganzzahlen"""
+class_mapping =  {label:idx for idx, label in
+                  enumerate(np.unique(df['classlabel']))}
+
+df['classlabel'] = df['classlabel'].map(class_mapping)
+ 
+#Rückgängig machen
+inv_class_mapping = {v: k for k, v in class_mapping.items()}
+#df['classlabel'] = df['classlabel'].map(inv_class_mapping)
+
+#andere Möglichkeit zum codieren
+class_le = LabelEncoder()
+y = class_le.fit_transform(df['classlabel'].values)
+#print(class_le.inverse_transform(y))
