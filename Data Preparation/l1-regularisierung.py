ba1868c610f78c2b67ca2f4930d08c0d269c4776
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-11-16 08:59:24 +0100

    plotted l1 regularization for wine data

--- Data Preparation/l1-regularisierung.py
+++ Data Preparation/l1-regularisierung.py
@@ -11,6 +11,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
 df_wine.columns = ['Class label', 'Alkohol', 
@@ -56,3 +58,37 @@ positiv rest negativ, 3. Spalte 3. Klasse positiv, rest negativ"""
 """lr.coef_ enthält 3 Zeilen und die 13 Merkmale als Spalten - diese
 werden mit den Merkmalen multipliziert um Nettoeingabe zu berechnen"""
 #print(lr.coef_)
+
+fig = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+weights, params = [], []
+
+for c in np.arange(-4.0, 6.0):
+    lr = LogisticRegression(penalty='l1', 
+                            C=10**c,
+                            random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1]) #schauen uns hier nur Gewichtungen für Klasse zwei an?
+    params.append(10**c)
+    
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column+1],
+             color=color)
+
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficent')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+plt.show()
