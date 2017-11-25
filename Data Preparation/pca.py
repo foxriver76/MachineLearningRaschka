cc48723f7a2f9cddd1614655c228f9dd6a2f071a
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-11-25 13:13:03 +0100

    finished pca step by step

--- Data Preparation/pca.py
+++ Data Preparation/pca.py
@@ -40,3 +40,31 @@ plt.ylabel('Anteil an der Varianzaufklärung')
 plt.xlabel('Hauptkomponenten')
 plt.legend(loc='best')
 plt.show()
+
+"""Eigenwerte & Vektoren nach Größe sortieren"""
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+    for i in range(len(eigen_vals))]
+eigen_pairs.sort(key = lambda k: k[0], reverse=True)
+
+"""Größten beiden EVs ermitteln"""
+w = np.hstack((eigen_pairs[0][1][: , np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis]))
+print('Matrix W:\n', w)
+
+"""Projektionsmatrix nutzen um Daten abzubilden x'=x*W"""
+#X_train_std[0].dot(w)
+"""Jetzt für alle Daten"""
+X_train_pca = X_train_std.dot(w)
+
+
+"""Visualisierung der projizierten Daten"""
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0],
+                X_train_pca[y_train==l, 1],
+                c=c, label=l, marker=m)
+plt.xlabel('PC 1') 
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()   
