11f4477b377cca939787df3cc3538841bde00e7a
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-05 11:04:12 +0100

    added rbf kernel

---
+++ Data Preparation/kernel_pca.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 10:49:07 2017
+
+@author: moritz
+"""
+
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy as np
+
+def rbf_kernel(X, gamma, n_components):
+    """Implementierung der RBF-Kernel-PCA
+    
+    Parameter
+    ---------
+    X: {numpy ndarray}, shape = [n_samples, n_features]
+    
+    gamma: float
+        Parameter zum abstimmen des RBF-Kernels
+        
+    n_components: int
+        Anzahl der zurückgelieferten Hauptkomponenten
+        
+    Rückgabewert
+    ------------
+    X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+        Projizierte Datenmenge
+    """
+
+    # Paarweise quadratische euklidische Abstände in der
+    # NxN-dimensionalen Datenmenge berechnen
+    sq_dists = pdist(X, 'sqeuclidean')
+    
+    # Abstände paarweise in qudaratische Matrix umwandeln
+    mat_sq_dists = squareform(sq_dists)
+    
+    # Symmetrische Kernel-Matrix berechnen
+    K = exp(-gamma * mat_sq_dists)
+    
+    # Kernel -Matrix zentrieren
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) \
+                        + one_n.dot(K).dot(one_n)
+                    
+    # Paare aus Eigenwerten und Eigenvektoren der 
+    # zentrierten Kernel-Matrix ermitteln; numpy.eigh gibt
+    # diese sortiert zurück
+    eigvals, eigvecs = eigh(K)
+    
+    # Die k wichtigsten Eigenvektoren ermitteln
+    X_pc = np.column_stack((eigvecs[:, -i]
+        for i in range(1, n_components + 1)))
+    
+    return X_pc
+    
