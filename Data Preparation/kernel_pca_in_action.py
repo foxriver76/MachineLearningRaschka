d9a8f5f717cb2898489ba6f3d88d58306884a17e
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-07 14:39:16 +0100

    added kernel pca in action

--- Data Preparation/kernel_pca_in_action.py
+++ Data Preparation/kernel_pca_in_action.py
@@ -11,6 +11,7 @@ from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import numpy as np
+from matplotlib.ticker import FormatStrFormatter
 
 X, y = make_moons(n_samples=100, random_state=123)
 plt.scatter(X[y==0, 0], X[y==0, 1],
@@ -35,9 +36,27 @@ ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02,
           color='blue', marker='o', alpha=0.5)
 ax[0].set_xlabel('PC1')
 ax[0].set_ylabel('PC2')
-ax[1].set_ylin([-1, 1])
+ax[1].set_ylim([-1, 1])
 ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 plt.show()
 
 """Jetzt Kernel_PCA"""
+X_kpca = rbf_kernel(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1],
+  color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1],
+  color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50, 1))+0.2,
+  color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50, 1))-0.2,
+  color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
