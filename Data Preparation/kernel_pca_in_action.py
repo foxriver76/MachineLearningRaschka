b482df0a24b8443f88bd47bedcf6ebdd67dc992d
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-08 17:48:44 +0100

    added separate circle example to kernel pca

--- Data Preparation/kernel_pca_in_action.py
+++ Data Preparation/kernel_pca_in_action.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import numpy as np
 from matplotlib.ticker import FormatStrFormatter
+from sklearn.datasets import make_circles
 
 X, y = make_moons(n_samples=100, random_state=123)
 plt.scatter(X[y==0, 0], X[y==0, 1],
@@ -60,3 +61,33 @@ ax[1].set_xlabel('PC1')
 ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 plt.show()
+
+"""Separieren von Kreisen Beispiel"""
+X, y = make_circles(n_samples=1000,
+                    random_state=123, noise=0.1, factor=0.2)
+plt.scatter(X[y==0, 0], X[y==0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1],
+            color='blue', marker='o', alpha=0.5)
+plt.show()
+
+"""Zuerst wieder PCA"""
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1],
+          color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1],
+          color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0, 0], np.zeros((500,1))+0.02,
+          color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((500,1))-0.02,
+          color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
+
+"""Jetzt Kernel PCA"""
