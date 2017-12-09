36fd15f24b87e454223e31e1a95cbeac31e5f312
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-09 11:29:45 +0100

    plottet circles with kernel pca

--- Data Preparation/kernel_pca_in_action.py
+++ Data Preparation/kernel_pca_in_action.py
@@ -91,3 +91,19 @@ ax[1].set_xlabel('PC1')
 plt.show()
 
 """Jetzt Kernel PCA"""
+X_kpca = rbf_kernel(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_spca[y==0, 1],
+          color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_spca[y==1, 1],
+          color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((500,1))+0.02,
+          color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((500,1))-0.02,
+          color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
