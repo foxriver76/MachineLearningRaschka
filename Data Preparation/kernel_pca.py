0c37e5a87bd5a4939228aa4161b98edc6cf39907
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-11 09:08:05 +0100

    reprojected points with kernel pca and pltotted them

--- Data Preparation/kernel_pca.py
+++ Data Preparation/kernel_pca.py
@@ -56,4 +56,61 @@ def rbf_kernel(X, gamma, n_components):
         for i in range(1, n_components + 1)))
     
     return X_pc
+
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """Implementierung der RBF-Kernel-PCA
+    die auch Eigenwerte der Kernelmatrix zurückgibt
     
+    Parameter
+    ---------
+    X: {numpy ndarray}, shape = [n_samples, n_features]
+    
+    gamma: float
+        Parameter zum abstimmen des RBF-Kernels
+        
+    n_components: int
+        Anzahl der zurückgelieferten Hauptkomponenten
+        
+    Rückgabewert
+    ------------
+    X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+        Projizierte Datenmenge
+    """
+
+    # Paarweise quadratische euklidische Abstände in der
+    # NxN-dimensionalen Datenmenge berechnen
+    sq_dists = pdist(X, 'sqeuclidean')
+    
+    # Abstände paarweise in qudaratische Matrix umwandeln
+    mat_sq_dists = squareform(sq_dists)
+    
+    # Symmetrische Kernel-Matrix berechnen
+    K = exp(-gamma * mat_sq_dists)
+    
+    # Kernel -Matrix zentrieren
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) \
+                        + one_n.dot(K).dot(one_n)
+                    
+    # Paare aus Eigenwerten und Eigenvektoren der 
+    # zentrierten Kernel-Matrix ermitteln; numpy.eigh gibt
+    # diese sortiert zurück
+    eigvals, eigvecs = eigh(K)
+    
+    # Die k wichtigsten Eigenvektoren ermitteln
+    alphas = np.column_stack((eigvecs[:, -i]
+        for i in range(1, n_components + 1)))
+    
+    # Korrespondierende Eigenwerte ermitteln
+    lambdas = [eigvals[-i] for i in range(1, n_components+1)]
+    
+    return alphas, lambdas
+    
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum(
+            (x_new-row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    
+    return k.dot(alphas / lambdas)
--- Data Preparation/kernel_pca_in_action.py
+++ Data Preparation/kernel_pca_in_action.py
@@ -6,7 +6,7 @@ Created on Wed Dec  6 08:15:13 2017
 @author: moritz
 """
 
-from kernel_pca import rbf_kernel
+from kernel_pca import rbf_kernel, rbf_kernel_pca, project_x
 from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -107,3 +107,30 @@ ax[1].set_ylim([-1, 1])
 ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 plt.show()
+
+"""Halbmonde mit der neuen Kernel PCA um neue Datenpunkte projizieren  
+zu können"""
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+
+# neuer Datenpunkt
+x_new = X[25]
+x_proj = alphas[25] #ursprüngliche Projektion
+
+x_reproj = project_x(x_new, X,
+                     gamma=15, alphas=alphas, lambdas=lambdas)
+
+"""Projektion visualisieren"""
+plt.scatter(alphas[y==0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y==1, 0], np.zeros((50)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black',
+            label='Urpsürngliche Projektion Punkt X[25]',
+            marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green',
+            label='Neuzuordnung Punkt X[25]',
+            marker='x', s=500)
+plt.legend(scatterpoints=1)
+plt.show()
+
