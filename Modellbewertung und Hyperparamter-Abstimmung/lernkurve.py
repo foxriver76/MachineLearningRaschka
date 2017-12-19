d85f0575f359630617b1afabd8c0b657b2f9f414
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-19 20:49:59 +0100

    added learning curve

---
+++ Modellbewertung und Hyperparamter-Abstimmung/lernkurve.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 20:37:03 2017
+
+@author: moritz
+"""
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-' \
+                 'databases/breast-cancer-wisconsin/wdbc.data', header=None)
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.2, random_state=1)
+    
+pipe_lr = Pipeline([('scl', StandardScaler()),
+                    ('clf', LogisticRegression(
+                            penalty='l2', random_state=0))])
+train_sizes, train_scores, test_scores = \
+    learning_curve(estimator=pipe_lr,
+                   X=X_train,
+                   y=y_train,
+                   train_sizes=np.linspace(0.1, 1.0, 10),
+                   cv=10,
+                   n_jobs=1)
+    
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(train_sizes, train_mean,
+         color='blue', marker='o',
+         markersize=5,
+         label='Korrektklassifizierung Training')
+plt.fill_between(train_sizes,
+                 train_mean + train_std,
+                 train_mean - train_std,
+                 alpha=0.15, color='blue')
+plt.plot(train_sizes, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='Korrektklassifizierung Validierung')
+plt.fill_between(train_sizes,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+plt.grid()
+plt.xlabel('Anzahl der Trainingsdaten')
+plt.ylabel('Korrektklassifizierungsrate')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.0])
+plt.show()
