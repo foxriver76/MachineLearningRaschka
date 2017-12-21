10d94887a4de45512fc1211e6bd7df81c4353e39
Moritz Heusinger <moritz.heusinger@gmail.com>
2017-12-21 10:02:05 +0100

    added validation curve

--- Modellbewertung und Hyperparamter-Abstimmung/validation_curve.py
+++ Modellbewertung und Hyperparamter-Abstimmung/validation_curve.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 19 20:37:03 2017
+Created on Thu Dec 21 09:52:21 2017
 
 @author: moritz
 """
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
-from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-' \
                  'databases/breast-cancer-wisconsin/wdbc.data', header=None)
@@ -29,36 +29,38 @@ X_train, X_test, y_train, y_test = \
 pipe_lr = Pipeline([('scl', StandardScaler()),
                     ('clf', LogisticRegression(
                             penalty='l2', random_state=0))])
-train_sizes, train_scores, test_scores = \
-    learning_curve(estimator=pipe_lr,
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = \
+    validation_curve(estimator=pipe_lr,
                    X=X_train,
                    y=y_train,
-                   train_sizes=np.linspace(0.1, 1.0, 10),
-                   cv=10,
-                   n_jobs=1)
+                   param_name='clf__C',
+                   param_range=param_range,
+                   cv=10)
     
 train_mean = np.mean(train_scores, axis=1)
 train_std = np.std(train_scores, axis=1)
 test_mean = np.mean(test_scores, axis=1)
 test_std = np.std(test_scores, axis=1)
-plt.plot(train_sizes, train_mean,
+plt.plot(param_range, train_mean,
          color='blue', marker='o',
          markersize=5,
          label='Korrektklassifizierung Training')
-plt.fill_between(train_sizes,
+plt.fill_between(param_range,
                  train_mean + train_std,
                  train_mean - train_std,
                  alpha=0.15, color='blue')
-plt.plot(train_sizes, test_mean,
+plt.plot(param_range, test_mean,
          color='green', linestyle='--',
          marker='s', markersize=5,
          label='Korrektklassifizierung Validierung')
-plt.fill_between(train_sizes,
+plt.fill_between(param_range,
                  test_mean + test_std,
                  test_mean - test_std,
                  alpha=0.15, color='green')
 plt.grid()
-plt.xlabel('Anzahl der Trainingsdaten')
+plt.xscale('log')
+plt.xlabel('Parameter C')
 plt.ylabel('Korrektklassifizierungsrate')
 plt.legend(loc='lower right')
 plt.ylim([0.8, 1.0])
