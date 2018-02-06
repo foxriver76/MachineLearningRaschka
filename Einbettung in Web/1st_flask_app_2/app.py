4fc9b35b3a67303619b99020a27a8663644afc2c
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-02-06 11:11:58 +0100

    created a second flask web application

---
+++ Einbettung in Web/1st_flask_app_2/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 10:42:12 2018
+
+@author: moritz
+"""
+
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+
+app = Flask(__name__)
+
+class HelloForm(Form):
+    sayhello = TextAreaField('',[validators.DataRequired()])
+
+@app.route('/')
+def index():
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form=form)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = request.form['sayhello']
+        return render_template('hello.html', name=name)
+    return render_template('first_app.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
