3c6dcba20ef757bb5cac9a9b1daedb54806c9cb8
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-02-17 11:33:33 +0100

    added linear regression and plotted scatterplot of lr

---
+++ Lineare Regression/linear_regression_gd_in_action.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 11:08:55 2018
+
+@author: moritz
+"""
+
+from linear_regression_gd import LinearRegressionGD, lin_regplot
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+"""Lebensbedingungen-Daten lesen"""
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/' \
+                 'housing/housing.data',
+                 header=None,
+                 sep='\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+X = df[['RM']].values
+y = df['MEDV'].values
+
+"""Standardisieren für bessere Konvergenz"""
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+
+X_std = sc_x.fit_transform(X)
+y_std = sc_y.fit_transform(y[:, np.newaxis]).flatten()
+
+lr = LinearRegressionGD()
+lr.fit(X_std, y_std)
+
+"""Konvergenz plotten -> Epochen vs Summe quad. Abweichungen"""
+plt.plot(range(1, lr.n_iter+1), lr.cost_)
+plt.ylabel('Summe quadrierter Abweichungen')
+plt.xlabel('Epochen')
+plt.show()
+
+"""Plotten des Streudiagramms zur Beurteilung"""
+lin_regplot(X_std, y_std, lr)
+plt.xlabel('Durchschnittliche Anzahl der Zimmer [RM] (standardisiert)')
+plt.ylabel('Preis in 1000$ [MEDV] (standardisiert)')
+plt.show()
