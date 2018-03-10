366f0fb9a0ce0f15a95a13ee28c236179f5fbcb0
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-03-10 09:45:05 +0100

    added kmeans clustering for unsupervised learning

---
+++ Clusteranalyse/k_means.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 10 09:14:55 2018
+
+@author: moritz
+"""
+
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+"""Create Sample Data"""
+X, y = make_blobs(n_samples=150,
+                  centers=3,
+                  n_features=2,
+                  cluster_std=0.5,
+                  shuffle=True,
+                  random_state=0)
+
+"""Plotten der Daten"""
+plt.scatter(X[:, 0], X[:, 1],
+            c='black',
+            marker='o',
+            s=50)
+plt.grid()
+plt.show()
+
+
+"""k_means auf die Daten anwenden"""
+km = KMeans(n_clusters=3, 
+             init='random',
+             n_init=10,
+             max_iter=300,
+             tol=1e-04,
+             random_state=0)
+
+y_km = km.fit_predict(X)
+
+plt.scatter(X[y_km==0, 0], X[y_km==0, 1],
+            c='b',
+            marker='o',
+            s=50)
+plt.scatter(X[y_km==1, 0], X[y_km==1, 1],
+            c='r',
+            marker='o',
+            s=50)
+plt.scatter(X[y_km==2, 0], X[y_km==2, 1],
+            c='lightgreen',
+            marker='o',
+            s=50)
+
+plt.grid()
+plt.show()
+
