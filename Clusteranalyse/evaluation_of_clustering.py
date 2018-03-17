540af9f8beff8bf6b8c1d2ab2fba1d9f3157d5dc
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-03-17 11:14:06 +0100

    added cluster with bad bias to show an example of bad silhouettecoefficent

--- Clusteranalyse/evaluation_of_clustering.py
+++ Clusteranalyse/evaluation_of_clustering.py
@@ -83,3 +83,37 @@ plt.yticks(yticks, cluster_labels + 1)
 plt.ylabel('Cluster')
 plt.xlabel('Silhouettenkoeffizient')
 plt.show() 
+
+"""Vergleichsweise schlechtes Clustering k=2"""
+km = KMeans(n_clusters=2,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+y_km = km.fit_predict(X)
+
+plt.scatter(X[y_km==0,0],
+            X[y_km==0,1],
+            s=50, c='lightgreen',
+            marker='s',
+            label='Cluster 1')
+
+plt.scatter(X[y_km==1,0],
+            X[y_km==1,1],
+            s=50, c='orange',
+            marker='o',
+            label='Cluster 2')
+
+plt.scatter(km.cluster_centers_[:, 0],
+            km.cluster_centers_[:, 1],
+            s=250,
+            marker='*',
+            c='red',
+            label='Zentroiden')
+
+plt.legend()
+plt.grid()
+plt.show()
+
+"""Silhouettendiagramm erstellen"""
