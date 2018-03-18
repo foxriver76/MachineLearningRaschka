6f1c7d8ed3d235bea8264758f111399f31bd9da3
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-03-18 11:50:22 +0100

    added silhouette plot for worse clustering

--- Clusteranalyse/evaluation_of_clustering.py
+++ Clusteranalyse/evaluation_of_clustering.py
@@ -117,3 +117,31 @@ plt.grid()
 plt.show()
 
 """Silhouettendiagramm erstellen"""
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+silhouette_vals = silhouette_samples(X, y_km, metric='euclidean')
+
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhouette_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper),
+             c_silhouette_vals,
+             height=1.0,
+             edgecolor='none',
+             color=color)
+    yticks.append((y_ax_lower + y_ax_upper) / 2)
+    y_ax_lower += len(c_silhouette_vals)
+    
+silhouette_avg = np.mean(silhouette_vals)
+plt.axvline(silhouette_avg,
+            color='red',
+            linestyle='--')
+plt.title('Schlechter Wert für Silhouettenkoeffizient - unterschiedliche Länge & Breite')
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouettenkoeffizient')
+plt.show() 
