3c293f1b1085f931e6b00054296ca1a09877186e
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-03-27 10:33:25 +0200

    prep for dbscan
    -used kmeans and hierarical clustering on data for compartion
    -plotted results of them

--- Clusteranalyse/dbscan.py
+++ Clusteranalyse/dbscan.py
@@ -8,6 +8,7 @@ Created on Mon Mar 26 15:32:01 2018
 
 from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, AgglomerativeClustering
 
 """Sample Daten erzeugen"""
 X, y = make_moons(n_samples=200,
@@ -15,4 +16,43 @@ X, y = make_moons(n_samples=200,
                   random_state=0)
 
 plt.scatter(X[:, 0], X[:, 1])
+plt.show()
+
+"""kmeans und agglomerative plotten zum Vergleich"""
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,3))
+km = KMeans(n_clusters=2,
+            random_state=0)
+y_km = km.fit_predict(X)
+ax1.scatter(X[y_km==0,0],
+            X[y_km==0,1],
+            c='lightblue',
+            marker='o',
+            s=40,
+            label='Cluster 1')
+ax1.scatter(X[y_km==1,0],
+            X[y_km==1,1],
+            c='red',
+            marker='s',
+            s=40,
+            label='Cluster 2')
+ax1.set_title('k-Means Clustering')
+
+ac = AgglomerativeClustering(n_clusters=2,
+                             affinity='euclidean',
+                             linkage='complete')
+y_ac = ac.fit_predict(X)
+ax2.scatter(X[y_ac==0,0],
+            X[y_ac==0,1],
+            c='lightblue',
+            marker='o',
+            s=40,
+            label='Cluster 1')
+ax2.scatter(X[y_ac==1,0],
+            X[y_ac==1,1],
+            c='red',
+            marker='s',
+            s=40,
+            label='Cluster 2')
+ax2.set_title('Agglomeratives Clustering')
+plt.legend()
 plt.show()
