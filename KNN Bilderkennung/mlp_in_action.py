7ea58f6eaf59c8c807045ab7858388f2cc447b92
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-04-10 14:53:35 +0200

    preparation for mlp classification
    -added class neuronales netz
    -added file for execution

---
+++ KNN Bilderkennung/mlp_in_action.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 14:49:19 2018
+
+@author: moritz
+"""
+
+from neuronales_netz import NeuralNetMLP
+
+"""784-50-10-MLP initialisieren (784 Eingabeeinheiten (n_features) +  50 verdeckte
+Einheiten (n_hidden) + 10 Ausgabeeinheiten (n_outputs)))"""
