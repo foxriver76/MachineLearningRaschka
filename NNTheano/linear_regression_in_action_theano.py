02e35d9bcdcfb88fa478b63334b05106b4a84402
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-05-04 08:17:57 +0200

    implemented lin reg in theano
    -implemented linear regression in theano
    -plotted results

--- NNTheano/linear_regression_in_action_theano.py
+++ NNTheano/linear_regression_in_action_theano.py
@@ -8,7 +8,8 @@ Created on Tue May  1 11:12:39 2018
 
 import numpy as np
 import theano
-from theano import tensor as T
+from linear_regression_theano import train_linreg, predict_linreg
+import matplotlib.pyplot as plt
 
 """Datenmenge erzeugen"""
 X_train = np.asarray([[0.0], [1.0],
@@ -24,3 +25,27 @@ y_train = np.asarray([1.0, 1.3,
                       6.6, 7.4,
                       8.0, 9.0],
                     dtype=theano.config.floatX)
+
+
+"""Linear Regression"""
+costs, w = train_linreg(X_train, y_train, 
+                        eta=0.001, epochs=10)
+plt.plot(range(1, len(costs)+1), costs)
+plt.tight_layout()
+plt.xlabel('Epoche')
+plt.ylabel('Straffunktion')
+plt.show()
+
+plt.scatter(X_train, 
+            y_train,
+            marker='s',
+            s=50)
+plt.plot(range(X_train.shape[0]),
+         predict_linreg(X_train, w),
+         color='gray',
+         marker='o',
+         markersize=4,
+         linewidth=3)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
