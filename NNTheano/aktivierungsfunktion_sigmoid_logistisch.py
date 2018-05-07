58b06241b97f0fe1589c1ea54e6de44872b5f4a7
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-05-07 15:01:27 +0200

    used logistic function
    -shows absolute % for class relation

--- NNTheano/aktivierungsfunktion_sigmoid_logistisch.py
+++ NNTheano/aktivierungsfunktion_sigmoid_logistisch.py
@@ -26,5 +26,31 @@ def logistic_activation(X, w):
 print('P(y=1|x) = %.3f'
       % logistic_activation(X, w)[0]) 
 # Wahrscheinlichkeit das Objekt x zur positiven Klasse gehört
+# Man bekommt so jedoch keine Bruchteile von 100% 
 
 """Klassenzugehörigkeit für jede Klasse in % berechnen"""
+# W : array, shape = [n_output_units, n_hidden_units+1]
+#          Weight matrix for hidden layer -> output layer.
+# note that first column (A[:][0] = 1) are the bias units
+W = np.array([[1.1, 1.2, 1.3, 0.5],
+              [0.1, 0.2, 0.4, 0.1],
+              [0.2, 0.5, 2.1, 1.9]])
+
+# A : array, shape = [n_hidden+1, n_samples]
+#          Activation of hidden layer.
+# note that first element (A[0][0] = 1) is for the bias units
+
+A = np.array([[1.0],
+              [0.1],
+              [0.3],
+              [0.7]])
+
+# Z : array, shape = [n_output_units, n_samples]
+#          Net input of output layer.
+
+Z = W.dot(A)
+y_probas = logistic(Z)
+print('Probabilities:\n', y_probas)
+
+y_class = np.argmax(Z, axis=0)
+print('predicted class label: %d' % y_class[0])
