883d4c9f60996e5cbf665b2f44966c2f87a86c89
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-05-09 08:16:39 +0200

    finished activation function overview
    -compared logistic activation with tanh function
    -plotted this

--- NNTheano/aktivierungsfunktion_sigmoid_logistisch.py
+++ NNTheano/aktivierungsfunktion_sigmoid_logistisch.py
@@ -7,6 +7,8 @@ Created on Sun May  6 11:09:34 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import expit
 
 """Beispiel"""
 X =  np.array([[1, 1.4, 1.5]])
@@ -54,3 +56,60 @@ print('Probabilities:\n', y_probas)
 
 y_class = np.argmax(Z, axis=0)
 print('predicted class label: %d' % y_class[0])
+
+"""Wahrscheinlichkeit der Klassenzugehörigkeit mit softmax Funktion abschätzen"""
+def softmax(z):
+    return np.exp(z) / np.sum(np.exp(z))
+
+def softmax_activation(X, w):
+    z = net_input(X, w)
+    return softmax(z)
+
+y_probas = softmax(Z)
+print('Probabilities:\n', y_probas)
+
+print('Sum of probabilities', y_probas.sum())
+
+y_class = np.argmax(Z, axis=0)
+print('Predicted class', y_class)
+
+"""Tangens hyperbolicus Aktivierungsfunktion [-1, 1] statt [0, 1] wie Logistik"""
+def tanh(z):
+    e_p = np.exp(z)
+    e_m = np.exp(-z)
+    return (e_p - e_m) / (e_p + e_m)
+
+z = np.arange(-5, 5, 0.005)
+log_act = logistic(z)
+tanh_act = tanh(z)
+
+# alternatives:
+# from scipy.special import expit
+# log_act = expit(z)
+# tanh_act = np.tanh(z)
+
+plt.ylim([-1.5, 1.5])
+plt.xlabel('net input $z$')
+plt.ylabel('activation $\phi(z)$')
+plt.axhline(1, color='black', linestyle='--')
+plt.axhline(0.5, color='black', linestyle='--')
+plt.axhline(0, color='black', linestyle='--')
+plt.axhline(-1, color='black', linestyle='--')
+
+plt.plot(z, tanh_act,
+         linewidth=2,
+         color='black',
+         label='tanh')
+plt.plot(z, log_act,
+         linewidth=2,
+         color='lightgreen',
+         label='logistic')
+
+plt.legend(loc='lower right')
+# plt.tight_layout()
+# plt.savefig('./figures/activation.png', dpi=300)
+plt.show()
+
+"""tanh und logistische Funktion sind schon vor implementiert"""
+tanh_act = np.tanh(z)
+log_act = expit(z)
